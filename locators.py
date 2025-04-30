a7796e507929b5bd0d0b4b7fab07c84d4b0589e3
Aman-F8 <amanullahf8@gmail.com>
2025-04-30 15:18:10 +0530

    updated

--- locators.py
+++
@@ -1,8 +0,0 @@
-class WebLocators:
-
-    Username = "user-name"
-    Password = "password"
-    Login_button = "login-button"
-    Menu_Button = "react-burger-menu-btn"
-    Logout = "logout_sidebar_link"
-    Cart = "shopping_cart_link"
